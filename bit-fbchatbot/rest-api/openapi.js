31fe6fe1a3e4b144baaad543c858fe4f006f7d79
eomjinyoung <jinyoung.eom@gmail.com>
2017-11-22 19:31:43 +0900

    .

--- bit-fbchatbot/rest-api/openapi.js
+++ bit-fbchatbot/rest-api/openapi.js
@@ -1,18 +1,18 @@
 const request = require('request');
 
-const searchNewAddress = (searchWord) => {
+const searchNewAddress = (type, searchWord) => {
     var uri = 'http://openapi.epost.go.kr/postal/retrieveNewAdressAreaCdService/retrieveNewAdressAreaCdService/getNewAddressListAreaCd';
     /* Service Key*/
-    var queryString = '?ServiceKey=' + 'swGnUeGbwJTEP%2FoOuEZsMstoBza7CSMZhJ%2FQGdasAbhNX5oUrxUn8sSJ3YjY3H0Og39RlBLPXtKWmxqRKjHAwQ%3D%3D';
+    var queryString = '?ServiceKey=' + process.env.OPENAPI_KEY;
 
     /* dong : 동(읍/면)명 road :도로명[default] post : 우편번호 */
-    queryString += '&searchSe=road';
+    queryString += '&searchSe=' + type;
 
     /* 검색어 */
     queryString += '&srchwrd=' + encodeURIComponent(searchWord); 
 
     /* 페이지당 출력될 개수를 지정 */
-    queryString += '&currentPerPage=10';
+    queryString += '&countPerPage=10';
 
     /* 출력될 페이지 번호 */
     queryString += '&currentPage=1';
@@ -26,7 +26,7 @@ const searchNewAddress = (searchWord) => {
     });  
 };
 
-searchNewAddress('금화로82번길 17');
+searchNewAddress('post', '17072');
 
 /*
 module.exports = {
