a1adc71a01cfb94f5cee5ccc7108282692efec7d
eomjinyoung <jinyoung.eom@gmail.com>
2017-11-27 20:58:49 +0900

    .

--- bit-fbchatbot/rest-api/openapi.js
+++ bit-fbchatbot/rest-api/openapi.js
@@ -25,26 +25,30 @@ const searchNewAddress = (type, searchWord, callback) => {
         //console.log('=> Headers', JSON.stringify(response.headers));
         console.log('=> Reponse received', body);
         parseString(body, (err, result) => {
-            var headers = result.NewAddressListResponse.cmmMsgHeader[0];
-            var totalCount = headers.totalCount[0];
-            var countPerPage = headers.countPerPage[0];
-            var currentPage = headers.currentPage[0];
-        
-            console.log('[주소 검색 결과]')
-            console.log(totalCount);
-            console.log(countPerPage);
-            console.log(currentPage);
-            console.log('------------------------------');
-        
-            var message = '';
-            var addrList = result.NewAddressListResponse.newAddressListAreaCd;
-            for (var addr of addrList) {
-                message += '[' + addr.zipNo[0] + ']\n';
-                message += addr.rnAdres[0] + '\n';
-                message += addr.lnmAdres[0] + '\n';
-                message += '\n';
+            try {
+                var headers = result.NewAddressListResponse.cmmMsgHeader[0];
+                var totalCount = headers.totalCount[0];
+                var countPerPage = headers.countPerPage[0];
+                var currentPage = headers.currentPage[0];
+            
+                console.log('[주소 검색 결과]')
+                console.log(totalCount);
+                console.log(countPerPage);
+                console.log(currentPage);
+                console.log('------------------------------');
+            
+                var message = '';
+                var addrList = result.NewAddressListResponse.newAddressListAreaCd;
+                for (var addr of addrList) {
+                    message += '[' + addr.zipNo[0] + ']\n';
+                    message += addr.rnAdres[0] + '\n';
+                    message += addr.lnmAdres[0] + '\n';
+                    message += '\n';
+                }
+                callback(message);
+            } catch (err) {
+                callback('주소 검색을 할 수 없습니다.');
             }
-            callback(message);
         });
     });  
 };
