3bf09f2b19395c99585ecd71b7bd39bafbb6ea44
Jinyoung Eom <jinyoung.eom@gmail.com>
2017-12-08 20:27:17 +0900

    .

--- bit-iot-python/examples/device.py
+++ bit-iot-python/examples/device.py
@@ -1,9 +1,49 @@
-import serial
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import logging
 import time
-ser = serial.Serial("/dev/ttyACM0")
-time.sleep(5)
-print("start....")
+import argparse
+import json
 
-while True:
-    print(ser.readline())
-    print("------------------------------------")
+AllowedActions = ['both', 'publish', 'subscribe']
+
+# Custom MQTT message callback
+def customCallback(client, userdata, message):
+    print("Received a new message: ")
+    print(message.payload)
+    print("from topic: ")
+    print(message.topic)
+    print("--------------\n\n")
+
+host = "a222gw6ygk2ekk.iot.ap-northeast-2.amazonaws.com"
+rootCAPath = "root-CA.crt"
+certificatePath = "led1.cert.pem"
+privateKeyPath = "led1.private.key"
+useWebsocket = False
+clientId = "clien0002"
+topic = "topic_1"
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect and subscribe to AWS IoT
+myAWSIoTMQTTClient.connect()
+myAWSIoTMQTTClient.subscribe(topic, 1, customCallback)
+time.sleep(2)
+
