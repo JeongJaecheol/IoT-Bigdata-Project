a138d8371f624eda8491510b6736823473caac36
JeongJaecheol <jcjong12110@gmail.com>
2017-12-26 19:04:11 +0900

    .

--- bit-iot-py/shadow.py
+++ bit-iot-py/shadow.py
@@ -19,10 +19,15 @@ def getCallback(payload, responseStatus, token):
     # AWSIoT 서버에서 받은 JSON 문자열을 객체로 변환
     dict = json.loads(payload)
     
-    if responseStatus == "rejected" and dict.code == 404:
+    if responseStatus == "rejected" and dict['code'] == 404:
         print("섀도우가 존재하지 않습니다.")
+
+        # shadow 기본 값 설정하기
+        myJSONPayload = '{"state":{"desired":{"led":"on"}}}'
+        myDeviceShadow.shadowUpdate(myJSONPayload, updateCallback, 5)
     else:
-        print(dict['state']['desired']['led'])
+        print("현재 섀도우의 값은 다음과 같습니다.")
+        print("led:" + dict['state']['desired']['led'])
         print(token)
     print("--------------")
 
@@ -77,6 +82,3 @@ myDeviceShadow = myShadowClient.createShadowHandlerWithName("dev01", True)
 # 현재 shadow 값 가져오기
 myDeviceShadow.shadowGet(getCallback, 5)
 
-# shadow 값 변경하기
-#myJSONPayload = '{"state":{"desired":{"led":"on"}}}'
-#myDeviceShadow.shadowUpdate(myJSONPayload, updateCallback, 5)
